093b4bca6c8e6ac9dc3048eedc0d7125d84243d6
John Ciaramella <58240787+Jciaramella96@users.noreply.github.com>
2020-10-27 21:09:20 -0400

    more code

---
+++ LootTable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// tell monobehavior to serialize
+[System.Serializable]
+//custom class
+public class Loot
+{
+    public PowerUp thisLoot;
+    public int lootChance;
+
+}
+
+[CreateAssetMenu]
+public class LootTable : ScriptableObject
+{
+    public Loot[] loots;
+
+   public PowerUp LootPowerUp()
+    {
+        //cumulative probability quick n dirty
+        int cumProb = 0;
+        int currentProb = Random.Range(0, 100); // random prob between 0 and 100
+        for(int i=0; i<loots.Length; i++)
+        {
+            cumProb += loots[i].lootChance;
+            if(currentProb <= cumProb)
+            {
+                return loots[i].thisLoot;
+            }
+        }
+        return null;
+    }
+}
---
+++ VectorValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
+{
+    // value running in game
+    public Vector2 initialValue;
+    // value by default when starting
+    public Vector2 defaultValue;
+
+    public void OnAfterDeserialize()
+    {
+        initialValue = defaultValue;
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
+
+
+}
