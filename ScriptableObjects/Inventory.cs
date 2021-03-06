9bec509fd4ba0ce81c6ef9f6b9b7c7c010c77e49
John Ciaramella <58240787+Jciaramella96@users.noreply.github.com>
2021-03-06 02:44:15 -0500

    Add files via upload

---
+++ ScriptableObjects/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+[System.Serializable]
+public class Inventory :ScriptableObject
+{
+    public Item currentItem;
+    public List<Item> items = new List<Item>();
+    public int numberOfKeys;
+    public float maxMagic = 15;
+    public float currentMagic;
+    public void OnEnable()
+    {
+        currentMagic = maxMagic;
+    }
+    public void ReduceMagic(float magicCost)
+    {
+        currentMagic -= magicCost;
+    }
+    public void AddItem(Item itemToAdd)
+    {
+
+        //istheitema key
+        if (itemToAdd.isKey)
+        {
+            numberOfKeys++;
+
+        }
+        else
+        {
+            if (!items.Contains(itemToAdd))
+            {
+                items.Add(itemToAdd);
+            }
+        }
+    }
+
+    public bool CheckForItem(Item item)
+    {
+        if (items.Contains(item))
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+        //possible clear inventory method, start game with all your stuff, find babur, lose, lose memory, have to regain everything in order to fight him again.
+}
---
+++ StateMachine/AI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+
+public class AI : MonoBehaviour
+{
+    public bool switchState = false;
+    public float gameTimer;
+    public int seconds = 0;
+    public Animator animator;
+    public FloatValue maxHealth;
+    public float currentHP;
+    public StateMachine<AI> stateMachine { get; set; }
+
+    private void Start()
+    {
+        stateMachine = new StateMachine<AI>(this);
+        stateMachine.ChangeState(FirstState.Instance);
+        gameTimer = Time.time;
+        currentHP = maxHealth.RunTimeValue;
+    }
+
+    private void Update()
+    {
+        if (currentHP <= 4)
+        {
+            animator.SetBool("Attack", true);
+            
+        }
+       
+        if (Time.time > gameTimer + 1)
+        {
+            gameTimer = Time.time;
+            seconds++;
+            Debug.Log(seconds);
+        }
+
+
+
+        if (seconds == 2)
+        {
+
+            // seconds = 0;
+            animator.SetBool("Attack", true);
+
+           
+        }
+        if (seconds == 5)
+        {
+
+            seconds = 0;
+            animator.SetBool("Attack", false);
+
+            switchState = !switchState;
+        }
+        stateMachine.Update();
+        //animator.SetBool("Attack", false);
+        
+    }
+}
